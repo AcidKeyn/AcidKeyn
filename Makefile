0024bf45cc27d27a92099b567b1d145d76874ecd
Ahmet Toslu <mrt@Air-von-Ahmet.lan>
2023-01-26 10:28:31 +0100

    Master upload

---
+++ Makefile
@@ -0,0 +1,2 @@
+giraffe: main.c
+	gcc main.c -o hello
---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define INPUT_LENGTH 30
+#define ALPHABET 25
+
+int main () {
+
+    bool condition=true;
+    
+    char smallAlpha[ALPHABET];
+    char bigAlpha[ALPHABET];
+    
+    while(condition) {
+        char word[INPUT_LENGTH+1];
+        
+        printf("Please Enter your Word for Hangman\n");
+        scanf("%s", word);
+
+        /*Check if Chars are only Alphabet*/
+        for(int i=0; i<strlen(word); i++) {
+            if((word[i]<'a' || word[i]>'z') && (word[i]<'A' || word[i]>'Z')) {
+                printf("Only alphabet allowed\n");
+                condition=true;
+                break;
+            } else {
+                condition=false;
+            }
+        }
+
+
+
+
+
+    }
+
+    
+
+    printf("END OF WHILE");
+    return 0;
+}
